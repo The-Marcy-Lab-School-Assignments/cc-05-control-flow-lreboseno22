0ddfafb602fc0a495afbba0554a218aacac613fd
lreboseno22 <liamrebo22@gmail.com>
2021-10-06 14:34:04 +0000

    Completed

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(num){
+  if (typeof num === "number" && num > 0) {
+      return num;
+  } else if (typeof num === "number" && num < 0) {
+      num *= -1;
+      return num;
+  } else {
+      return null;
+  }
 }
 
+console.log(absoluteValue(-5));
 
+function multiplesOfFourOrSix() {
+  let array = [];
+    for (let i = 1; i <= 100; i++) {
+      if (i % 4 === 0 && i % 6 === 0){
+          array.push(`${i}`);
+        }
+    }
+    return array;
+}
 
-
+console.log(multiplesOfFourOrSix());
